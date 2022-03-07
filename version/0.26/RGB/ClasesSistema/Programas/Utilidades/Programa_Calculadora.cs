ee81a8c07e6b98a82dcb2bad45d25b6de9ab65a3
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2022-03-07 18:59:30 -0500

    a
    a

--- version/0.26/RGB/ClasesSistema/Programas/Utilidades/Programa_Calculadora.cs
+++ version/0.26/RGB/ClasesSistema/Programas/Utilidades/Programa_Calculadora.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace RGB.ClasesSistema.Programas.Utilidades
 {
-    internal class Programa_Calculadora
+    public class Programa_Calculadora
     {
+        //Atributos
         private long valor_a;
         private long valor_b;
         private long resultado;
+
+        //Constructores
+        public Programa_Calculadora(long valor_a, long valor_b, long resultado)
+        {
+            Valor_a = valor_a;
+            Valor_b = valor_b;
+            Resultado = resultado;
+        }
+        //Accesores
+        public long Valor_a { get => valor_a; set => valor_a = value; }
+        public long Valor_b { get => valor_b; set => valor_b = value; }
+        public long Resultado { get => resultado; set => resultado = value; }
+
+        //Metodos
+        public long Sumar(long a, long b)
+        {
+            Resultado = a + b;
+            return Resultado;
+        }
+        public long Restar(long a, long b)
+        {
+
+        }
     }
 }
